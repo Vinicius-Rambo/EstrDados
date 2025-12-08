ebdc1d5b438786bbc1172478a8e50e0b46b09448
Vinicius Rambo <vncs.rambo@gmail.com>
2025-12-08 09:41:25 -0300

    Adicionado um menu e corrigido alguns prints

--- 11 - Ordenação/main.c
+++ 11 - Ordenação/main.c
@@ -32,8 +32,30 @@ void mostrarVetor(int *dados, int tam){
     }
 }
 
+int menu(){
+
+    int opc;
+    do{
+        printf("\n- - - Menu - - -\n");
+        printf("[1] - Mostrar valores do vetor\n");
+        printf("[2] - Tempo por QuickSortSort\n");
+        printf("[3] - Tempo por MergeSort\n");
+        printf("[4] - Tempo por BubbleSortt\n");
+        printf("[5] - Todos os tempos\n");
+        printf("[0] - Sair\n");
+        printf("Escolha uma das opções: ");
+        scanf("%d", &opc);
+        if (opc <0 || opc > 5) {
+            printf("Entrada invalida\n");
+        }
+
+    }while(opc < 0 || opc > 5);
+
+    return opc;
+}
+
 int main(){
-    int tam;
+    int tam, opcao;
     
     //Variaveis para o cronometro
     clock_t ini, fim;
@@ -52,38 +74,73 @@ int main(){
 
     //Vetores estáticos (VLA)
     int dados[tam], bub[tam], mer[tam], aux[tam], qui[tam];
-        
     *dados = inserirVetor(dados, tam);
-
-    mostrarVetor(dados, tam);
-
+    
     //Copia os valores aleatórios para cada vetor
     for(int i = 0; i < tam; i++){
         bub[i] = dados[i];
         mer[i] = dados[i];
         qui[i] = dados[i];
     }
-    
-    //MergeSort
-    ini = clock();
-    mergeSort(mer, aux, 0, tam - 1);
-    fim = clock();
-    tempo = (double)(fim - ini) / CLOCKS_PER_SEC;
-    printf("Merge     : %f s\n", tempo);
 
-    // QuickSort 
-    ini = clock();
-    quickSort(qui, 0, tam - 1);
-    fim = clock();
-    tempo = (double)(fim - ini) / CLOCKS_PER_SEC;
-    printf("QuickSort : %f s\n", tempo);
+    do{
+        opcao = menu();
+        system("clear");
+        switch (opcao) {  
+            
+            case 1:
+                mostrarVetor(dados, tam);
+                break;
+            
+            case 2: 
+                ini = clock();
+                quickSort(qui, 0, tam - 1);
+                fim = clock();
+                tempo = (double)(fim - ini) / CLOCKS_PER_SEC;
+                printf("QuickSort : %f s\n", tempo);
+                break;
 
-    //BubbleSort
-    ini = clock();
-    bubbleSort(bub, tam);
-    fim = clock();
-    tempo = (double)(fim - ini) / CLOCKS_PER_SEC;
-    printf("Bubble    : %f s\n", tempo);
-    
+            case 3:
+                ini = clock();
+                mergeSort(mer, aux, 0, tam - 1);
+                fim = clock();
+                tempo = (double)(fim - ini) / CLOCKS_PER_SEC;
+                printf("Merge     : %f s\n", tempo);
+                break;
+            
+            case 4:
+                ini = clock();
+                bubbleSort(bub, tam);
+                fim = clock();
+                tempo = (double)(fim - ini) / CLOCKS_PER_SEC;
+                printf("Bubble    : %f s\n", tempo);
+                break;
+            
+            case 5: 
+                ini = clock();
+                quickSort(qui, 0, tam - 1);
+                fim = clock();
+                tempo = (double)(fim - ini) / CLOCKS_PER_SEC;
+                printf("QuickSort : %f s\n", tempo);
+                
+                ini = clock();
+                mergeSort(mer, aux, 0, tam - 1);
+                fim = clock();
+                tempo = (double)(fim - ini) / CLOCKS_PER_SEC;
+                printf("Merge     : %f s\n", tempo);
+                
+                ini = clock();
+                bubbleSort(bub, tam);
+                fim = clock();
+                tempo = (double)(fim - ini) / CLOCKS_PER_SEC;
+                printf("Bubble    : %f s\n", tempo);
+                break;
+            
+            default: 
+                printf("Bye... \n");
+                break;
+        }
+    }while (opcao != 0); 
     return 0;
-}
+  
+  }
